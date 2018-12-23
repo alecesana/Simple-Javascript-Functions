b592ed065ad34543e6cdb39486c72f33027dee15
Ale Cesana <39651347+alecesana@users.noreply.github.com>
2018-12-23 22:46:32 +1100

    Update myFilter function.js

--- myFilter function.js
+++ myFilter function.js
@@ -5,14 +5,10 @@ Note: A pure function is allowed to alter local variables defined within its sco
 
 Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method.*/
 var s = [23, 65, 98, 5];
-
 Array.prototype.myFilter = function(callback){
-  var newArray = [];
-   
-  this.forEach(function(x){if(callback(x)==true){newArray.push(x)}})
-  
+  var newArray = [];   
+  this.forEach(function(x){if(callback(x)==true){newArray.push(x)}})  
   return newArray;
-
 };
 
 var new_s = s.myFilter(function(item){
